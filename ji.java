c9dc81f9fda4a5bdbda3751cdf9ab7c9f89f7239
Ronaldo19850205 <dj20250602@dge.go.kr>
2026-03-26 15:33:08 +0900

    Add files via upload

---
+++ ji.java
@@ -0,0 +1,15 @@
+package jump2java;
+
+public class ji {
+	public static void main(String[]args) {
+		int a = 0;
+		while (a < 10) {
+		    a++;
+		    if (a % 2 == 1) {
+		        continue;  // 짝수인 경우 조건문으로 돌아간다.
+		    }
+		    System.out.println(a);  // 홀수만 출력된다.
+		}
+
+	}
+}
